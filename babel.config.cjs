29542da96a8fcfcda205eb46c601684354d50fed
Vusivious <118552158+programmingwithvusi@users.noreply.github.com>
2026-06-19 23:48:10 +0200

    remove all files

--- babel.config.cjs
+++
@@ -1,2 +0,0 @@
-// Empty Babel config to avoid loading presets during test runs in this workspace
-module.exports = {};
--- jest.config.cjs
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
-};
