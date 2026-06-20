59aecfb18418d23f82773dc271d8ecfd018657e3
Vusivious <118552158+programmingwithvusi@users.noreply.github.com>
2026-06-20 09:03:20 +0200

    reset: clean troubleshooting artifacts; browser-first CI + Netlify workflow

--- tools/validate_yaml.py
+++
@@ -1,9 +0,0 @@
-﻿import yaml,traceback,sys
-f = r"C:/Projects/Dev/imr4724/SciCalcApp/.github/workflows/ci.yml"
-try:
-    yaml.safe_load(open(f))
-    print("YAML_OK")
-except Exception as e:
-    print("YAML_ERROR", e)
-    traceback.print_exc()
-    sys.exit(1)
